5a428727f33834bb1318d6d07b8fbc6f7752da13
Arseny M <marseny42@ya.ru>
2021-12-03 21:53:46 +0300

    this should work

--- src/api.js
+++ src/api.js
@@ -2,15 +2,14 @@ import store from '@/store';
 import router from './router';
 
 export function setUserTelegramData(data) {
-  store.dispatch('setUserTelegramData', data);
-  console.log(store.getters.user)
-  console.log(store.getters.user?.account.firstName)
-  if (!store.getters.user?.account.firstName) {
-    store.dispatch('setUserAccountInfo', {
-      firstName: data.first_name,
-      lastName: data?.last_name,
-    });
-  }
+  store.dispatch('setUserTelegramData', data).then(() => {
+    if (!store.getters.user?.account.firstName) {
+      store.dispatch('setUserAccountInfo', {
+        firstName: data.first_name,
+        lastName: data?.last_name,
+      });
+    }
+  })
 }
 
 export function getUser() {
