f9a10af6e30d39b369a90bdb24d707b747a8db37
Arseny M <marseny42@ya.ru>
2021-12-11 18:45:19 +0300

    hint buttons in safari

--- src/components/common/TextInput.vue
+++ src/components/common/TextInput.vue
@@ -23,9 +23,7 @@
           </svg>
 
           <div v-else-if="hint && !loading" class="input__hint">
-            <button type="button" class="input__hint-button plain" v-if="hint" tabindex="-1">
-              <QuestionBold/>
-            </button>
+            <button type="button" class="input__hint-button plain" v-if="hint" tabindex="-1"/>
             <div class="input__hint-text">
               <div class="input__hint-bubble"/>
               {{ hint }}
@@ -47,7 +45,6 @@
 
 <script setup>
 import { ref, toRefs } from 'vue'
-import QuestionBold from '@/assets/icons/question-bold.svg'
 
 const props = defineProps({
   modelValue: [String, Number],
@@ -285,11 +282,12 @@ defineExpose({ focus })
       height 18px
       border-radius 50%
       background-color #c0c4c7
+      background-image url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12'%3E%3Cpath fill='white' d='M 5.84 8.03 C 6.32 8.03 6.63 7.81 6.78 7.35 C 6.84 7.11 6.95 6.91 7.09 6.76 C 7.24 6.6 7.52 6.41 7.94 6.17 C 8.41 5.89 8.75 5.59 8.98 5.25 C 9.2 4.91 9.31 4.51 9.31 4.05 C 9.31 3.28 9.02 2.66 8.44 2.2 C 7.86 1.73 7.08 1.5 6.12 1.5 C 5.42 1.5 4.81 1.62 4.29 1.86 C 3.78 2.11 3.41 2.44 3.2 2.85 C 3.07 3.08 3 3.34 3 3.61 C 3 3.89 3.08 4.12 3.26 4.29 C 3.43 4.46 3.67 4.55 3.97 4.55 C 4.45 4.55 4.78 4.34 4.96 3.92 C 5.16 3.46 5.51 3.23 6.01 3.23 C 6.3 3.23 6.53 3.31 6.72 3.47 C 6.9 3.63 6.99 3.84 6.99 4.1 C 6.99 4.38 6.92 4.6 6.77 4.78 C 6.63 4.96 6.34 5.18 5.91 5.43 C 5.55 5.63 5.28 5.86 5.1 6.11 C 4.93 6.35 4.84 6.64 4.84 6.98 L 4.84 6.98 L 4.84 7.02 C 4.84 7.32 4.93 7.57 5.11 7.75 C 5.3 7.94 5.54 8.03 5.84 8.03 Z M 5.88 10.95 C 6.21 10.95 6.49 10.84 6.72 10.62 C 6.96 10.4 7.08 10.13 7.08 9.81 C 7.08 9.49 6.96 9.22 6.72 9 C 6.49 8.78 6.21 8.67 5.88 8.67 C 5.54 8.67 5.26 8.78 5.03 9 C 4.79 9.22 4.68 9.49 4.68 9.81 C 4.68 10.13 4.79 10.4 5.03 10.62 C 5.26 10.84 5.54 10.95 5.88 10.95 Z'/%3E%3C/svg%3E%0A")
+      background-repeat no-repeat
+      background-position center
+      background-size 12px
       color white
       cursor help
-
-      svg
-        width 12px
 
     &-text
       display flex
